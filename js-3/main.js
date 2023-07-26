1318a7e83f14b8fc788f81a58b4450b7fc0c4c72
000.goz <116011109+Samandar0070@users.noreply.github.com>
2023-07-26 21:25:48 +0500

    Add files via upload

---
+++ js-3/main.js
@@ -0,0 +1,18 @@
+
+let pul
+console.log(money = prompt("do you have money ?")) 
+
+let samalyot = 500 * 11000.34
+hotel = 250 * 11000.34
+museum = 120 * 12354
+
+let result = airplane + hotel + museum
+console.log(result + " so'm")
+
+let natija = money - result 
+
+if(natija >= 0){
+    console.log("Oq yo'l, Alisher")
+} else if(natija <= 0){
+    console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan.")
+}
